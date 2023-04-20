816730a4f49bb750fec308b29c64727bc65ba40f
Maicolt401 <maicolt401@gmail.com>
2023-04-20 22:53:52 +0200

    added new menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="menu-header">
+        <img
+          class="profile-icon"
+          src="https://thumb.tildacdn.com/tild3662-6661-4764-b331-373839393230/-/resize/360x/-/format/webp/logo_negro_la_villa.png"
+          alt="Mark Lee"
+        />
+      </div>
+      <nav>
+        <button class="nav-button">
+          <i class="fa-sharp fa-solid fa-table-columns"></i>{" "}
+          <a href="https://www.lavillasants.com/la-villa-sants?table_number=1&delivery=restaurant">
+            {" "}
+            CARTA
+          </a>
+        </button>
+        <button class="nav-button">
+          <i class="fa-sharp fa-regular fa-pen-to-square"></i>
+          <a href="https://profee.link/DSseKQ?r=eyJjIjoiZXVyIn0&flow=tips">
+            PROPINA
+          </a>
+        </button>
+        <button class="nav-button">
+          <i class="fa-sharp fa-solid fa-bolt"></i>
+          <a href="https://search.google.com/local/writereview?placeid=ChIJu0Wr_IWYpBIROJGP4dNWyT4">
+            RESEÑAS EN GOOGLE
+          </a>
+        </button>
+        <button class="nav-button">
+          <i class="fa-regular fa-envelope"></i>
+          <a href="https://www.tripadvisor.es/UserReviewEdit-d4600261">
+            {" "}
+            RESEÑAS EN TRIPADVISOR{" "}
+          </a>
+        </button>
+        <button class="nav-button">
+          <i class="fa-regular fa-heart"></i>
+          <a href="https://instagram.com/lavillasants">SIGUENOS EN INSTAGRAM</a>
+        </button>
+      </nav>
+    </main>
   );
 }
 
